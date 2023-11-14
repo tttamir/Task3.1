bb8592bc154d29b325233ea401bb8e58e0830168
tttamir <tamircook1003@gmail.com>
2023-11-14 20:50:00 +0300

    added world morre

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,8 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 }
